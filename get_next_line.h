e2929129bfca378df8e24c3f4448bf7c6c9bc1f7
chilituna <133121967+chilituna@users.noreply.github.com>
2023-07-18 20:11:49 +0200

    Add files via upload
    still issue with francinette "limits"

---
+++ get_next_line.h
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aarponen <aarponen@student.42berlin.de>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/16 08:43:09 by aarponen          #+#    #+#             */
+/*   Updated: 2023/07/18 17:07:07 by aarponen         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <stddef.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 32
+# endif
+
+char	*get_next_line(int fd);
+char	*copy_new_line(char *line);
+char	*ft_read_line(int fd, char *reading_line);
+char	*go_to_next_line(char *line);
+size_t	ft_strlen(const char *str);
+char	*ft_strjoin(const char *s1, const char *s2);
+char	*pre_ft_strjoin(char *line, char *temp_buffer);
+char	*ft_strchr(const char *s, int c);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_strdup(const char *s);
+
+#endif
